a879471283909e9d3afd38117179452bf2f6cea6
TheChufas <facundoagustinluduena@upc.edu.ar>
2026-09-15 02:23:53 -0300

    Add main menu widget and controller bindings
    Add a new MainMenuWidget with actions for searching a lobby, creating a lobby, and exiting the game, along with a username display. The player controller is updated to use the new menu widget, and the widget binds button click handlers in NativeConstruct.

---
+++ Source/LedgeOut/Private/Widgets/MainMenuWidget.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/Widgets/MainMenuWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+void UMainMenuWidget::NativeConstruct()
+{
+	// Bind Buttons to functions
+	SearchLobby_Button->OnClicked.AddDynamic(this, &UMainMenuWidget::SearchLobby);
+	CreateLobby_Button->OnClicked.AddDynamic(this, &UMainMenuWidget::HostLobby);
+	ExitGame_Button->OnClicked.AddDynamic(this, &UMainMenuWidget::ExitGame);
+	
+	// Retriving Username
+	Username_TextBlock->SetText(FText::FromString(TEXT("Xx_Pablosaurio_xX")));
+}
+
+void UMainMenuWidget::SearchLobby()
+{
+	UE_LOG(LogTemp, Log, TEXT("Buscando lobby..."));
+}
+
+void UMainMenuWidget::HostLobby()
+{
+	UE_LOG(LogTemp, Log, TEXT("Creando lobby..."));
+}
+
+void UMainMenuWidget::ExitGame()
+{
+	UE_LOG(LogTemp, Log, TEXT("Saliendo del juego"));
+}
---
+++ Source/LedgeOut/Public/Widgets/MainMenuWidget.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuWidget.generated.h"
+
+class UButton;
+class UTextBlock;
+
+/**
+ * 
+ */
+UCLASS()
+class LEDGEOUT_API UMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(Category = "Buttons", EditDefaultsOnly, meta = (BindWidget))
+	TObjectPtr<UButton>  SearchLobby_Button;
+	
+	UPROPERTY(Category = "Buttons", EditDefaultsOnly, meta = (BindWidget))
+	TObjectPtr<UButton>  CreateLobby_Button;
+	
+	UPROPERTY(Category = "Buttons", EditDefaultsOnly, meta = (BindWidget))
+	TObjectPtr<UButton>  ExitGame_Button;
+	
+	UPROPERTY(Category = "Text", EditDefaultsOnly, meta = (BindWidget))
+	TObjectPtr<UTextBlock> Username_TextBlock;
+	
+	UFUNCTION()
+	void SearchLobby();
+	
+	UFUNCTION()
+	void HostLobby();
+	
+	UFUNCTION()
+	void ExitGame();
+
+protected:
+	virtual void NativeConstruct() override;
+};
